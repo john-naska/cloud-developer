6e60c767611bef408450a0b1999f6a19e68f6fca
John Naska <john.naska@gmail.com>
2020-04-10 13:43:12 +0200

    Updated files
    Updated files with previously edited ones

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -25,7 +25,7 @@ import { Car, cars as cars_list } from './cars';
   // > try it {{host}}/persons/:the_name
   app.get( "/persons/:name", 
     ( req: Request, res: Response ) => {
-      let { name } = req.params;
+      let { name } = req.params; // john: this means basically "unpack the variables in 'req.params' into the variable 'name' "
 
       if ( !name ) {
         return res.status(400)
@@ -71,12 +71,64 @@ import { Car, cars as cars_list } from './cars';
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
 
+  app.get( "/cars/", ( req: Request, res: Response ) => {
+    let { make } = req.query;
+
+    let cars_list2 = cars;
+
+    if (make) {
+      cars_list2 = cars.filter((car) => car.make === make);
+    }
+
+    return res.status(200)
+              .send(cars_list2);
+  } );
+
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
 
+  app.get("/cars/:id", (req: Request, res: Response) => {
+    let {id} = req.params;
+
+    if (!id) {
+      return res.status(400).send('id is required');
+    }
+
+    const car = cars.filter((car) => car.id == id);
+
+    if(car && car.length === 0) {
+      return res.status(404)
+              .send('car is not found');
+    }
+
+    res.status(200).send(car);
+    
+  });
+
+
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+
+  app.post( "/cars/", 
+  async ( req: Request, res: Response ) => {
+
+    let { make, type, model, cost, id } = req.body;
+
+    if ( !make || !type || !model || !cost || !id) {
+      return res.status(400)
+                .send(`make, type, model, cost and id are required`);
+    }
+
+    const new_car: Car = {
+      make:make, type:type, model:model, cost:cost, id:id 
+    };
+
+    cars.push(new_car);
+
+    res.status(201)
+              .send(new_car);
+} );
 
   // Start the Server
   app.listen( port, () => {
--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -38,3 +38,16 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+
+
+describe('concat', () => {
+
+  it('should concat \'Hello\' and \'World\'', () => {
+    expect(() => {concat('Hello', 'World')}).to.equal('Hello World');
+  });
+
+  it('should throw and error if one string is empty', () => {
+    expect(() => {concat("Hello ", "")}).to.throw('No empty Strings please!');
+  });
+
+});
